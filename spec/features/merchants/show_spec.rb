834ce0e08d5b4cd18acbc9b2f394920748949de1
Sam Iavri <sammi80134@gmail.com>
2022-04-04 17:19:42 -0600

    All tests passing, refactor

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe 'Merchant Show Page' do
     it 'has a title and list items', :vcr do
       visit merchant_path(1)
 
-      expect(page).to have_content("Schroeder-Jerde's Items")
+      expect(page).to have_content('Schroeder-Jerde')
+      expect(page).to have_content('Item Nemo Facere')
     end
   end
 end
