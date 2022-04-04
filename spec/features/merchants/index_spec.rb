5507434bd9d5090aa0061c3d93ddf936dfa4ee55
Sam Iavri <sammi80134@gmail.com>
2022-04-04 12:25:26 -0600

    Index test set up, gemfile and spec helper updated

---
+++ spec/features/merchants/index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Index Page' do
+  describe 'View' do
+    it 'has a title and links to merchants', :vcr do
+      visit merchants_path
+
+      expect(page).to have_content('Merchants')
+
+      click_link('Schroeder-Jerde')
+      expect(current_path).to eq('/merchants/1')
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -61,4 +61,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.default_cassette_options = { re_record_interval: 1.days }
 end
